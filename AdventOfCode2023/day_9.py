eb67ceb81b89d839fac697b3d3740c7f64bc53bf
Nicole Gentile <nicole.gentile@mail.polimi.it>
2023-12-11 21:19:52 +0100

    Day 9 puzzle 2 solutions

--- AdventOfCode2023/day_9.py
+++ AdventOfCode2023/day_9.py
@@ -49,9 +49,42 @@ def puzzle_1_solution():
                 # extract the end of each sequence
                 final_value = extract_end_of_sequence(sequences)
                 values.append(final_value)
-
                 break
     return sum(values)
 
+
+def extract_start_of_sequence(sequences: List[List[int]]) -> int:
+    # start from the last sequence
+    new_value = None
+    for i in range(len(sequences) - 1, 0, -1):
+        current_sequence = sequences[i]
+        prev_sequence = sequences[i - 1]
+        new_value = prev_sequence[0] - current_sequence[0]
+        prev_sequence.insert(0, new_value)
+
+    return new_value
+
+
+def puzzle_2_solution():
+    report = get_report(day_9_input)
+    values = []
+    for line in report:
+        prev_sequence = line
+        sequences = [line]
+        while True:
+            next_sequence = generate_next_sequence(prev_sequence)
+            sequences.append(next_sequence)
+            prev_sequence = next_sequence
+
+            # If all elements are 0, find the end of each sequence
+            if set(next_sequence) == {0}:
+                # extract the end of each sequence
+                final_value = extract_start_of_sequence(sequences)
+                values.append(final_value)
+                break
+    print(values)
+    return sum(values)
+
+
 if __name__ == '__main__':
-    print(puzzle_1_solution())
+    print(puzzle_2_solution())
